bd888fdd91d7e326909f2bb84a5d0fd0f827a9f9
Park Juhyung <jh@kodebox.io>
2019-05-14 18:01:46 +0900

    Add "graph_network_out_all_node" API

--- server/src/common_rpc_types.rs
+++ server/src/common_rpc_types.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 use std::net::IpAddr;
 
+use chrono::{DateTime, Utc};
 use cprimitives::H256;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use serde_json;
 
 pub type NodeName = String;
@@ -103,4 +105,54 @@ pub enum UpdateCodeChainRequest {
         binary_url: String,
         binary_checksum: String,
     },
+}
+
+
+#[derive(Debug, Clone)]
+pub enum GraphPeriod {
+    Minutes5,
+    Hour,
+    Day,
+}
+
+impl Serialize for GraphPeriod {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer, {
+        serializer.serialize_str(match *self {
+            GraphPeriod::Minutes5 => "minutes5",
+            GraphPeriod::Hour => "hour",
+            GraphPeriod::Day => "day",
+        })
+    }
+}
+
+impl<'de> Deserialize<'de> for GraphPeriod {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>, {
+        let s = String::deserialize(deserializer)?;
+        Ok(match s.as_str() {
+            "minutes5" => GraphPeriod::Minutes5,
+            "hour" => GraphPeriod::Hour,
+            "day" => GraphPeriod::Day,
+            _ => unreachable!(),
+        })
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct GraphCommonArgs {
+    pub from: DateTime<Utc>,
+    pub to: DateTime<Utc>,
+    pub period: GraphPeriod,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GraphNetworkOutAllRow {
+    pub node_name: String,
+    pub time: DateTime<Utc>,
+    pub value: f32,
 }
